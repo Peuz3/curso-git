fadb0a071650caad4ce0377c3dccd373849cf163
Joao Pedro <jp.ssilva@live.com>
2018-08-20 15:44:38 -0300

    Acrescentado dois traços

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -6,10 +6,13 @@ namespace projeto_teste
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("--------------------------------------");
             Console.WriteLine("Olá mundo!");            
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia!");
             Console.WriteLine("Tchau!");
+            Console.WriteLine("--------------------------------------");
+
 
             Console.ReadLine();
         }
